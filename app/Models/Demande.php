b360a8ae104b0f46e44382501d80f6588cc85308
Corinabm <bellemarecorina@gmail.com>
2025-09-30 19:56:50 -0400

    model, migration table demande

---
+++ app/Models/Demande.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Demande extends Model
+{
+    protected $fillable = [
+        'plante_id',
+        'nom_plante',
+        'prix_unitaire',
+        'nom',
+        'prenom',
+        'telephone',
+        'email',
+        'horaires_indiques',
+        'date_recuperation',
+        'mode_recuperation',
+        'message',
+        'date_demande',
+        'quantite',
+        'statut',
+        'montant_total',
+        'email_envoye',
+        'date_envoi_email',
+        'statut_email'
+    ];
+    
+    protected $casts = [
+        'prix_unitaire' => 'decimal:2',
+        'montant_total' => 'decimal:2',
+        'date_recuperation' => 'date',
+        'date_demande' => 'date',
+        'date_envoi_email' => 'datetime',
+        'email_envoye' => 'boolean',
+        'quantite' => 'integer'
+    ];
+    
+    public function plante()
+    {
+        return $this->belongsTo(Plante::class, 'plante_id');
+    }
+}
