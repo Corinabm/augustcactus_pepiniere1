0a2418c2e196bdf946668579984c65312bc23104
Corinabm <bellemarecorina@gmail.com>
2025-09-30 23:35:13 -0400

    route (accueil, services, galerie, a propos, contact, nos plantes)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PlanteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Routes PageController
+Route::get('/', [PageController::class, 'accueil'])->name('accueil');
+Route::get('/services', [PageController::class, 'services'])->name('services');
+Route::get('/galerie', [PageController::class, 'galerie'])->name('galerie');
+Route::get('/a-propos', [PageController::class, 'aPropos'])->name('a-propos');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
+// Routes PlanteController
+Route::get('/nos-plantes', [PlanteController::class, 'index'])->name('plantes.index');
+Route::get('/nos-plantes/{plante}', [PlanteController::class, 'show'])->name('plantes.show');
